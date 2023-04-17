58c32228e157605791fcd070481daefcd2bec034
Reece <reece.williams7@gmail.com>
2023-04-17 22:14:33 +0100

    WIP

--- cmd/create-jwk/main.go
+++ cmd/create-jwk/main.go
@@ -1,56 +1,23 @@
 package main
 
 import (
-	"crypto/rand"
-	"crypto/rsa"
 	"log"
 
-	//jose "gopkg.in/square/go-jose.v2"
-
-	"github.com/lestrrat-go/jwx/v2/jwk"
+	"github.com/reecewilliams7/go-security-tools/internal/jsonWebKeys"
 )
 
 func main() {
-	// rsaKey, _ := rsa.GenerateKey(rand.Reader, 2048)           // XXX Check err
-	// serialNumber, _ := rand.Int(rand.Reader, big.NewInt(100)) // XXX Check err
-
-	// template := x509.Certificate{
-	// 	SerialNumber: serialNumber,
-	// 	Subject: pkix.Name{
-	// 		Organization: []string{"Example Co"},
-	// 	},
-	// 	NotBefore:             time.Now(),
-	// 	NotAfter:              time.Now().Add(2 * time.Hour),
-	// 	KeyUsage:              x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature,
-	// 	ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth},
-	// 	BasicConstraintsValid: true,
-	// }
-
-	// derBytes, _ := x509.CreateCertificate(rand.Reader, &template, &template, &rsaKey.PublicKey, rsaKey) // XXX Check err
-	// certificate, _ := x509.ParseCertificate(derBytes)
-
-	// jwk := jose.JSONWebKey{
-	// 	Certificates: []*x509.Certificate{certificate},
-	// 	Key:          &rsaKey.PublicKey,
-	// 	KeyID:        "someKeyId",
-	// 	Use:          "sig",
-	// }
-
-	// jwkJsonBytes, _ := jwk.MarshalJSON()
-	// jwkJson := string(jwkJsonBytes)
-
-	// log.Println(jwkJson)
-
-	var rawKey interface{}
-	rsaKey, err := rsa.GenerateKey(rand.Reader, 2048)
+	jc := jsonWebKeys.NewJsonWebKeyCreator()
+	o, err := jc.Create()
 	if err != nil {
-		log.Fatalf(`failed to generate rsa private key: %w`, err)
+		log.Fatalf("%s", err)
 	}
-	rawKey = rsaKey
 
-	key, err := jwk.FromRaw(rawKey)
-
-	if err != nil {
-		log.Fatalf(`failed to create new JWK from raw key: %w`, err)
-	}
+	log.Println(o.JsonWebKeyString)
+	log.Println("---------------------------")
+	log.Println(o.Base64JsonWebKey)
+	log.Println("---------------------------")
+	log.Println(o.RsaPrivateKey)
+	log.Println("---------------------------")
+	log.Println(o.RsaPublicKey)
 }
--- go.mod
+++ go.mod
@@ -2,6 +2,4 @@ module github.com/reecewilliams7/go-security-tools
 
 go 1.16
 
-require (
-	github.com/lestrrat-go/jwx/v2 v2.0.9
-)
+require github.com/lestrrat-go/jwx/v2 v2.0.9
